9bd9483ac597060e438dbb9fe652c11797c47db8
amit-lekhak <amitlekhak1@gmail.com>
2021-08-11 22:32:39 +0545

    Updates

---
+++ static/js/6.c4e4632d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsmart-brain"]=this["webpackJsonpsmart-brain"]||[]).push([[6],{329:function(i,n,s){"use strict";s.r(n);s(10),s(330);var e=s(3);n.default=function(i){var n=i.signoutHandler,s=i.profileClickHandler;return Object(e.jsxs)("div",{className:"dropdown",children:[Object(e.jsx)("div",{onClick:s,className:"dropdown-items",children:"Profile"}),Object(e.jsx)("hr",{}),Object(e.jsx)("div",{onClick:n,className:"dropdown-items",children:"Signout"})]})}},330:function(i,n,s){}}]);
+//# sourceMappingURL=6.c4e4632d.chunk.js.map
